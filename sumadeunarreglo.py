b1b45f7c1fb9849b5e30e05df8620a2f39550cbd
Martha <ada_hack279@hackademy.mx>
2022-03-27 01:29:40 -0600

    Agregué el archivo

---
+++ sumadeunarreglo.py
@@ -0,0 +1,18 @@
+
+#Suma de un arreglo
+#Teniendo un arreglo de enteros encuentra el resultado de la suma de los elementos del arreglo. El arreglo debe de ser menor a mil datos.
+
+lista = []
+
+num = int(input("Introduce el numero de datos del array: "))
+if num < 1000:
+
+  print("Introduce los elementos del array: ")
+
+else:
+		print("Opción inválida")
+
+for n in range(num):
+  numbers = int(input())
+  lista.append(numbers)
+print("Suma:", sum(lista))
